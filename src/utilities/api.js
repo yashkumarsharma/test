833afc30fad469fbac7e37951fd561264548bdf3
Nassim <nassim.amokrane@engineering.outlier.org>
2021-05-04 21:48:45 +0100

    feat: add login request

---
+++ src/utilities/api.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+import Config from 'react-native-config'
+
+export const login = async (username, password) => {
+  try {
+    const queryData = {
+      username,
+      password,
+      client_id: Config.AUTH0_CLIENT_ID,
+      grant_type: 'http://auth0.com/oauth/grant-type/password-realm',
+      realm: 'Username-Password-Authentication',
+    }
+
+    const { data } = await axios.post(`https://${Config.AUTH0_DOMAIN}/oauth/token`, queryData)
+    console.log(data)
+    return data
+  } catch (e) {
+    console.log(e.response.data)
+  }
+}
