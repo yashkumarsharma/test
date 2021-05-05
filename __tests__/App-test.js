4aab3f8f51d790db76fa74a5d1dd3f7aaedeaa40
Nassim <nassim.amokrane@engineering.outlier.org>
2021-05-05 22:50:36 +0100

    feat: configure tests for the app

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -1,14 +1,11 @@
-/**
- * @format
- */
+import React from 'react'
+import { render } from '@testing-library/react-native'
+import App from '../App'
 
-import 'react-native';
-import React from 'react';
-import App from '../App';
+test('Home should render OK', async () => {
+  const { container, toJSON } = render(
+    <App />,
+  )
 
-// Note: test renderer must be required after react-native.
-import renderer from 'react-test-renderer';
-
-it('renders correctly', () => {
-  renderer.create(<App />);
-});
+  expect(toJSON(container)).toMatchSnapshot()
+})
