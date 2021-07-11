78f79430bb80f893b0d27467540cc1a2221625ae
Yash Kumar Sharma <yash.sharma@engineering.outlier.org>
2021-07-11 19:56:02 +0530

    test

--- .github/script.sh
+++ .github/script.sh
@@ -3,13 +3,7 @@
 MESSAGE=$(git log -1 HEAD --pretty=format:%s)
 INPUT = "fix:hello"
 
-
-  
-
 SUBSTRING=$(echo $INPUT| cut -d':' -f 1) 
 
-
-
-
-if [[ "$SUBSTRING" == "fix" ]]; then npm version patch
-if [[ "$SUBSTRING" == "feat" ]]; then npm version minor
+if [[ "$SUBSTRING" == "fix" ]]; then npm version patch; fi
+if [[ "$SUBSTRING" == "feat" ]]; then npm version minor; fi
